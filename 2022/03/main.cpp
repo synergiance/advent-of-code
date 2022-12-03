7a74424440494371d80a4c3ed4592eeef8fc4b90
Synergiance <syn@synerfiles.info>
2022-12-03 01:11:21 -0500

    typo

--- 2022/03/main.cpp
+++ 2022/03/main.cpp
@@ -32,7 +32,7 @@ int main() {
 		if (buffer.empty()) continue;
 
 		if (buffer.size() % 2 != 0) {
-			std::cerr<<"Rupsack Contents Discrepency!"<<std::endl;
+			std::cerr<<"Rupsack Contents Discrepancy!"<<std::endl;
 		}
 
 		int halfBufferSz = buffer.size() / 2;
