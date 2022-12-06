e8cad039eaea9acf1c6d07eb9f464d2cf1886fb3
Synergiance <syn@synerfiles.info>
2022-12-06 00:29:55 -0500

    Day 6: Tuning Trouble

--- 2022/06/main.cpp
+++ 2022/06/main.cpp
@@ -2,30 +2,44 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <Parser.h>
-#include <Reader.h>
 
-using namespace Syn;
+static const int headerSz = 14;
 
 int main() {
-	std::ifstream iFile("supply_stacks.dat");
+	std::ifstream iFile("communication.dat");
 	if (!iFile.is_open()) {
 		std::cout<<"Could not open file!"<<std::endl;
 		return 1;
 	}
 
 	std::cout<<"File successfully opened!"<<std::endl;
-	std::string buffer;
 
-	// TODO: Pre parsing
+	char c;
+	char header[headerSz];
 
-	while (Reader::getline(iFile, buffer)) {
-		if (buffer.empty()) continue;
+	for (int i = 0; i < headerSz && iFile.get(c); i++)
+		header[i] = c;
 
-		// TODO: While Parsing
+	size_t charsRead = headerSz;
+
+	while (iFile.get(c)) {
+		header[charsRead % headerSz] = c;
+		bool duplicate = false;
+
+		for (int i = 1; i < headerSz; i++)
+			for (int j = 0; j < i; j++)
+				duplicate |= header[i] == header[j];
+
+		charsRead++;
+		if (!duplicate) break;
 	}
 
-	// TODO: Post parsing
+	std::string code;
+	for (int i = 0; i < headerSz; i++)
+		code.push_back(header[(charsRead + i) % headerSz]);
+
+	std::cout<<"Start of message: "<<code<<std::endl;
+	std::cout<<"Number of bytes read: "<<charsRead<<std::endl;
 
 	iFile.close();
 	return 0;
