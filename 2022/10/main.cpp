802d8e7af435ecb7be5bb8336f3a2ae4594a1341
Synergiance <syn@synerfiles.info>
2022-12-10 00:39:35 -0500

    Day 10 part 1

--- 2022/10/main.cpp
+++ 2022/10/main.cpp
@@ -7,8 +7,16 @@
 
 using namespace Syn;
 
+enum Instruction {
+	noop, addx
+};
+
+int samplePoints[] = {
+		20, 60, 100, 140, 180, 220
+};
+
 int main() {
-	std::ifstream iFile("input.dat");
+	std::ifstream iFile("cpu_instructions.dat");
 	if (!iFile.is_open()) {
 		std::cout<<"Could not open file!"<<std::endl;
 		return 1;
@@ -17,15 +25,38 @@ int main() {
 	std::cout<<"File successfully opened!"<<std::endl;
 	std::string buffer;
 
-	// TODO: Set up
+	int x = 1; // The CPU's only register
+	int cycle = 0;
 
-	while (Reader::getline(iFile, buffer)) {
-		if (buffer.empty()) continue;
+	int signalTotal = 0;
+	int sampleIdx = 0;
 
-		// TODO: Process
+	while (sampleIdx < 6) {
+		Instruction inst = noop;
+		int param = 0;
+		int instTime = 1;
+
+		if (Reader::getline(iFile, buffer)) {
+			std::vector<std::string> tokens;
+			Parser::Tokenize(buffer, tokens);
+
+			if (tokens.size() >= 2 && tokens[0] == "addx") {
+				inst = addx;
+				param = atoi(tokens[1].c_str());
+				instTime = 2;
+			}
+		}
+
+		cycle += instTime;
+
+		if (cycle >= samplePoints[sampleIdx])
+			signalTotal += x * samplePoints[sampleIdx++];
+
+		if (inst == addx)
+			x += param;
 	}
 
-	// TODO: Output
+	std::cout<<"Signal total: "<<signalTotal<<std::endl;
 
 	iFile.close();
 	return 0;
