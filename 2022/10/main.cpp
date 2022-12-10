cbdf0e56abe2d93203ed814359961bb60495509f
Synergiance <syn@synerfiles.info>
2022-12-10 17:51:47 -0500

    Refactor Day 10 using ElfProcessor library

--- 2022/10/main.cpp
+++ 2022/10/main.cpp
@@ -1,45 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <Parser.h>
 #include <Reader.h>
+#include <ElfProcessor.h>
 
 using namespace Syn;
-
-enum Func {
-	noop, addx
-};
-
-struct Instruction {
-	Func func;
-	int time;
-	int param;
-};
 
 const int screenWidth = 40;
 const int screenHeight = 6;
 const int screenArraySz = screenHeight * screenWidth;
-
-Instruction DecodeInstruction(const std::string &instStr) {
-	Instruction inst{noop, 1, 0};
-	std::vector<std::string> tokens;
-	Parser::Tokenize(instStr, tokens);
-
-	if (tokens.size() < 2 || tokens[0] == "noop")
-		return inst;
-
-	inst.func = addx;
-	inst.param = atoi(tokens[1].c_str());
-	inst.time = 2;
-
-	return inst;
-}
-
-void ApplyInstruction(const Instruction &inst, int &x) {
-	if (inst.func == noop) return;
-	x += inst.param;
-}
 
 bool CheckSprite (int screenPos, int spritePos) {
 	int relativePos = screenPos - spritePos;
@@ -56,26 +25,20 @@ int main() {
 	std::cout<<"File successfully opened!"<<std::endl;
 	std::string buffer;
 
-	int x = 1; // The CPU's only register
+	Elf::Processor communicatorProcessor;
+
+	while (Reader::getline(iFile, buffer)) {
+		if (buffer.empty()) continue;
+
+		communicatorProcessor.AddInstruction(Elf::Processor::DecodeInstruction(buffer));
+	}
 
 	char screenOutput[screenArraySz];
-	Instruction inst = {noop, 1, 0};
-	int instBegin = -1;
 
 	for (int cycle = 0; cycle < screenArraySz; cycle++) {
-		if (instBegin + inst.time - cycle == 0) {
-			ApplyInstruction(inst, x);
-
-			if (Reader::getline(iFile, buffer)) {
-				inst = DecodeInstruction(buffer);
-			} else {
-				inst = {noop, 1, 0};
-			}
-
-			instBegin = cycle;
-		}
-
-		screenOutput[cycle] = CheckSprite(cycle % screenWidth, x) ? '#' : '.';
+		communicatorProcessor.Cycle();
+		int spritePos = communicatorProcessor.GetRegisterX();
+		screenOutput[cycle] = CheckSprite(cycle % screenWidth, spritePos) ? '#' : '.';
 	}
 
 	std::string str;
