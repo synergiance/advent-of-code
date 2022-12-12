196bdfc5c3be33b6597f114806aa5878b1d37cf0
Synergiance <syn@synerfiles.info>
2022-12-12 00:09:19 -0500

    Starting point, day 12, Hill Climbing

--- 2022/12/main.cpp
+++ 2022/12/main.cpp
@@ -8,7 +8,7 @@
 using namespace Syn;
 
 int main() {
-	std::ifstream iFile("input.dat");
+	std::ifstream iFile("map.dat");
 	if (!iFile.is_open()) {
 		std::cout<<"Could not open file!"<<std::endl;
 		return 1;
