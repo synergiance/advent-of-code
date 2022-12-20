2ab638c88a83dd7dff1b0e80239cac133e86b4c0
Synergiance <syn@synerfiles.info>
2022-12-20 01:31:12 -0500

    Naive not correct answer for day 20 but passes example input somehow

--- 2022/20/main.cpp
+++ 2022/20/main.cpp
@@ -4,11 +4,23 @@
 #include <vector>
 #include <Parser.h>
 #include <Reader.h>
+#include <unordered_set>
 
 using namespace Syn;
 
+struct MixerValue {
+	int value;
+	bool hasMixed;
+};
+
+const int GroveCoordinates[] = {
+		1000,
+		2000,
+		3000
+};
+
 int main() {
-	std::ifstream iFile("input.dat");
+	std::ifstream iFile("encrypted_coordinates.dat");
 	if (!iFile.is_open()) {
 		std::cout<<"Could not open file!"<<std::endl;
 		return 1;
@@ -17,16 +29,58 @@ int main() {
 	std::cout<<"File successfully opened!"<<std::endl;
 	std::string buffer;
 
-	// TODO: Set up
+	std::vector<MixerValue> numbers;
 
 	while (Reader::getline(iFile, buffer)) {
 		if (buffer.empty()) continue;
 
-		// TODO: Process
+		int number = atoi(buffer.c_str());
+		numbers.push_back({number, false});
 	}
 
-	// TODO: Output
-
 	iFile.close();
+
+	std::cout<<"Loaded "<<numbers.size()<<" numbers."<<std::endl;
+
+	int currentPosition = 0;
+	while (currentPosition < numbers.size()) {
+		MixerValue currentVal = numbers[currentPosition];
+
+		if (currentVal.hasMixed) {
+			currentPosition++;
+			continue;
+		}
+
+		currentVal.hasMixed = true;
+
+		int newPosition = (currentVal.value + currentPosition) % (int)numbers.size();
+		if (newPosition < 0) newPosition += numbers.size();
+
+		int negativeOffset = currentVal.value > 0 ? 1 : 0;
+		int oldPosition = currentPosition;
+
+		if (newPosition != oldPosition)
+			numbers.insert(numbers.cbegin() + newPosition + negativeOffset, currentVal);
+		if (newPosition <= oldPosition)
+			currentPosition++;
+		if (newPosition != oldPosition)
+			numbers.erase(numbers.cbegin() + currentPosition);
+	}
+
+	int zeroPos = -1;
+	for (int i = 0; i < numbers.size(); i++) {
+		if (numbers[i].value != 0) continue;
+		zeroPos = i;
+		break;
+	}
+
+	std::cout<<"Zero position: "<<zeroPos<<std::endl;
+
+	int coordinateSum = 0;
+	for (int position : GroveCoordinates)
+		coordinateSum += numbers[(position + zeroPos) % numbers.size()].value;
+
+	std::cout<<"Coordinate sum: "<<coordinateSum<<std::endl;
+
 	return 0;
 }
