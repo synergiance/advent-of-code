d0db811d351fbe1b8ddeedbe6711cf6ec6d7e54f
Synergiance <syn@synerfiles.info>
2022-12-21 01:37:02 -0500

    Day 21 Part 2

--- 2022/21/main.cpp
+++ 2022/21/main.cpp
@@ -9,7 +9,7 @@
 using namespace Syn;
 
 enum MathOp {
-	none, add, subtract, multiply, divide
+	none, add, subtract, multiply, divide, equals
 };
 
 struct Monkey {
@@ -20,13 +20,24 @@ struct Monkey {
 	std::string rhs;
 };
 
+const std::string finalMonkey = "root";
+const std::string me = "humn";
+
 Monkey ParseMonkey(const std::string &input) {
 	Monkey monkey{};
 	std::vector<std::string> tokens;
 	Parser::Tokenize(input, tokens);
 	monkey.name = tokens[0].substr(0, 4);
 
-	if (tokens.size() == 2) {
+	if (monkey.name == me) {
+		monkey.num = -1;
+		monkey.op = none;
+	} else if (monkey.name == finalMonkey) {
+		monkey.lhs = tokens[1];
+		monkey.rhs = tokens[3];
+		monkey.op = equals;
+		monkey.num = 0;
+	} else if (tokens.size() == 2) {
 		monkey.num = atoi(tokens[1].c_str());
 		monkey.op = none;
 	} else {
@@ -51,6 +62,7 @@ Monkey ParseMonkey(const std::string &input) {
 		}
 		monkey.num = 0;
 	}
+
 	return monkey;
 }
 
@@ -61,6 +73,8 @@ long GetVal(const std::string &name, std::unordered_map<std::string, Monkey> &mo
 
 	long lhs = GetVal(currentMonkey.lhs, monkeys);
 	long rhs = GetVal(currentMonkey.rhs, monkeys);
+
+	if (lhs == -1 || rhs == -1) return -1;
 
 	switch (currentMonkey.op) {
 		case add:
@@ -84,6 +98,33 @@ long GetVal(const std::string &name, std::unordered_map<std::string, Monkey> &mo
 	return currentMonkey.num;
 }
 
+long GetEqualValue(long lhs, long rhs, long product, MathOp op) {
+	if (lhs < 0) {
+		switch (op) {
+			case add:
+				return product - rhs;
+			case subtract:
+				return product + rhs;
+			case multiply:
+				return product / rhs;
+			case divide:
+				return product * rhs;
+		}
+	} else if (rhs < 0) {
+		switch (op) {
+			case add:
+				return product - lhs;
+			case subtract:
+				return lhs - product;
+			case multiply:
+				return product / lhs;
+			case divide:
+				return lhs / product;
+		}
+	}
+	return -1;
+}
+
 int main() {
 	std::ifstream iFile("monkeys.dat");
 	if (!iFile.is_open()) {
@@ -103,7 +144,32 @@ int main() {
 		monkeys.insert({newMonkey.name, newMonkey});
 	}
 
-	std::cout<<"Root's value: "<<GetVal("root", monkeys)<<std::endl;
+	Monkey currentMonkey = monkeys[finalMonkey];
+	long product = GetVal(currentMonkey.lhs, monkeys);
+	std::string nextMonkey = currentMonkey.rhs;
+	if (product < 0) {
+		product = GetVal(currentMonkey.rhs, monkeys);
+		nextMonkey = currentMonkey.lhs;
+	}
+
+	while (nextMonkey != me) {
+		currentMonkey = monkeys[nextMonkey];
+
+		long lhs = GetVal(currentMonkey.lhs, monkeys);
+		long rhs = GetVal(currentMonkey.rhs, monkeys);
+
+		if (lhs < 0) {
+			nextMonkey = currentMonkey.lhs;
+		} else if (rhs < 0) {
+			nextMonkey = currentMonkey.rhs;
+		} else {
+			std::cout<<"Error: could not get monkey tree!"<<std::endl;
+			break;
+		}
+		product = GetEqualValue(lhs, rhs, product, currentMonkey.op);
+	}
+
+	std::cout<<"My value: "<<product<<std::endl;
 
 	iFile.close();
 	return 0;
