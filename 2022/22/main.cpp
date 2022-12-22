08e5968744e8a5bd902a81594a3a1c5085d3e468
Synergiance <syn@synerfiles.info>
2022-12-22 03:30:13 -0500

    Day 22 Part 2 not done

--- 2022/22/main.cpp
+++ 2022/22/main.cpp
@@ -15,6 +15,8 @@ const char gNullChar = ' ';
 const char gRight = 'R';
 const char gLeft = 'L';
 
+const int gCubeSize = 50;
+
 const int gRowMultiplier = 1000;
 const int gColumnMultiplier = 4;
 
@@ -32,6 +34,79 @@ const std::string gDirectionStr[] {
 	"Up"
 };
 
+struct CubeEdge {
+	int connectedFace;
+	int rotationOffset;
+};
+
+struct CubeFaceData {
+	CubeEdge edges[4];
+};
+
+/**
+ * .12
+ * .4.
+ * 67.
+ * 9..
+ */
+
+const CubeFaceData gCubeFaces[] {
+	{},
+	{
+		{
+			{2, 0},
+			{4, 0},
+			{6, 2},
+			{9, 1}
+		}
+	},
+	{
+		{
+			{7, 2},
+			{4, 1},
+			{1, 0},
+			{9, 0}
+		}
+	},
+	{},
+	{
+		{
+			{2, 3},
+			{7, 0},
+			{6, 3},
+			{1, 0}
+		}
+	},
+	{},
+	{
+		{
+			{7, 0},
+			{9, 0},
+			{1, 2},
+			{4, 1}
+		}
+	},
+	{
+		{
+			{2, 2},
+			{9, 1},
+			{6, 0},
+			{4, 0}
+		}
+	},
+	{},
+	{
+		{
+			{7, 3},
+			{2, 0},
+			{1, 3},
+			{6, 0}
+		}
+	},
+	{},
+	{}
+};
+
 enum InstructionType {
 	move, turn
 };
@@ -45,6 +120,19 @@ struct MapInstruction {
 	TurnDirection direction;
 	int moveDistance;
 };
+
+int GetCubeFaceIdx(Coordinate &coordinate) {
+	return coordinate.x / gCubeSize + (coordinate.y / gCubeSize) * 3;
+}
+
+Coordinate GetCubeOrigin(int cubeIdx) {
+	if (cubeIdx < 0 || cubeIdx >= 12)
+		std::cerr<<"Index out of bounds! "<<cubeIdx<<std::endl;
+	return {
+		(cubeIdx % 3) * gCubeSize,
+		cubeIdx * gCubeSize / 3
+	};
+}
 
 int main() {
 	std::ifstream iFile("monkey_map.dat");
@@ -123,18 +211,69 @@ int main() {
 
 		Coordinate directionOffset = gDirections[currentDirection];
 		Coordinate potentialPosition = currentPosition;
+		int potentialDirection = currentDirection;
 
 		for (int i = 0; i < instruction.moveDistance; i++) {
-			do {
-				potentialPosition += directionOffset;
-				potentialPosition.x %= map.GetWidth();
-				potentialPosition.y %= map.GetHeight();
-				if (potentialPosition.x < 0) potentialPosition.x += map.GetWidth();
-				if (potentialPosition.y < 0) potentialPosition.y += map.GetHeight();
-			} while (map[potentialPosition] == gNullChar);
+			potentialPosition += directionOffset;
+			potentialPosition.x %= map.GetWidth();
+			potentialPosition.y %= map.GetHeight();
+			if (potentialPosition.x < 0) potentialPosition.x += map.GetWidth();
+			if (potentialPosition.y < 0) potentialPosition.y += map.GetHeight();
+
+			if (map[potentialPosition] == gNullChar) {
+				int cubeIdx = GetCubeFaceIdx(currentPosition);
+				int nextCubeIdx = gCubeFaces[cubeIdx].edges[currentDirection].connectedFace;
+				int rotationOffset = gCubeFaces[cubeIdx].edges[currentDirection].rotationOffset;
+				int posOnEdge;
+
+				switch (currentDirection) {
+					case 0:
+						posOnEdge = currentPosition.y % gCubeSize;
+						break;
+					case 1:
+						posOnEdge = gCubeSize - (currentPosition.x % gCubeSize) - 1;
+						break;
+					case 2:
+						posOnEdge = gCubeSize - (currentPosition.y % gCubeSize) - 1;
+						break;
+					case 3:
+						posOnEdge = currentPosition.x % gCubeSize;
+						break;
+					default:
+						posOnEdge = 0;
+						break;
+				}
+
+				potentialDirection += rotationOffset;
+				potentialDirection %= 4;
+
+				Coordinate cubeOffset{0, 0};
+				switch (potentialDirection) {
+					case 0:
+						cubeOffset.y = posOnEdge;
+						break;
+					case 1:
+						cubeOffset.x = gCubeSize - posOnEdge - 1;
+						break;
+					case 2:
+						cubeOffset.y = gCubeSize - posOnEdge - 1;
+						cubeOffset.x = gCubeSize - 1;
+						break;
+					case 3:
+						cubeOffset.x = posOnEdge;
+						cubeOffset.y = gCubeSize - 1;
+						break;
+					default:
+						break;
+				}
+
+				potentialPosition = GetCubeOrigin(nextCubeIdx) + cubeOffset;
+			}
 
 			if (map[potentialPosition] == gWallChar) break;
 			currentPosition = potentialPosition;
+			currentDirection = potentialDirection;
+			directionOffset = gDirections[currentDirection];
 		}
 	}
 
